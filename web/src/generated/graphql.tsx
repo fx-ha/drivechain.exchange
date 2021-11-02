4aadee2c3aeef42e3de6cd804343419138808444
Felix Ha <felixha@protonmail.com>
2021-11-02 03:21:18 +0000

    refactor(web): :recycle: lint

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -1,85 +1,110 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
-export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions =  {}
+import { gql } from '@apollo/client'
+import * as Apollo from '@apollo/client'
+export type Maybe<T> = T | null
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K]
+}
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>
+}
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>
+}
+const defaultOptions = {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-};
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+}
 
 export type Block = {
-  __typename?: 'Block';
-  createdAt: Scalars['String'];
-  hash: Scalars['String'];
-  height: Scalars['Float'];
-};
+  __typename?: 'Block'
+  createdAt: Scalars['String']
+  hash: Scalars['String']
+  height: Scalars['Float']
+}
 
 export type NewsItem = {
-  __typename?: 'NewsItem';
-  block: Block;
-  content: Scalars['String'];
-  fee: Scalars['Float'];
-  topic: Topic;
-  txid: Scalars['String'];
-};
+  __typename?: 'NewsItem'
+  block: Block
+  content: Scalars['String']
+  fee: Scalars['Float']
+  topic: Topic
+  txid: Scalars['String']
+}
 
 export type Query = {
-  __typename?: 'Query';
-  newsByTopic: Array<NewsItem>;
-  topics: Array<Topic>;
-};
-
+  __typename?: 'Query'
+  newsByTopic: Array<NewsItem>
+  topics: Array<Topic>
+}
 
 export type QueryNewsByTopicArgs = {
-  topic: Scalars['String'];
-};
+  topic: Scalars['String']
+}
 
 export type Topic = {
-  __typename?: 'Topic';
-  createdAt: Scalars['String'];
-  hex: Scalars['String'];
-  name: Scalars['String'];
-};
+  __typename?: 'Topic'
+  createdAt: Scalars['String']
+  hex: Scalars['String']
+  name: Scalars['String']
+}
 
 export type NewsQueryVariables = Exact<{
-  topic: Scalars['String'];
-}>;
+  topic: Scalars['String']
+}>
 
+export type NewsQuery = {
+  __typename?: 'Query'
+  newsByTopic: Array<{
+    __typename?: 'NewsItem'
+    content: string
+    fee: number
+    txid: string
+    block: {
+      __typename?: 'Block'
+      createdAt: string
+      hash: string
+      height: number
+    }
+    topic: {
+      __typename?: 'Topic'
+      name: string
+      hex: string
+      createdAt: string
+    }
+  }>
+}
 
-export type NewsQuery = { __typename?: 'Query', newsByTopic: Array<{ __typename?: 'NewsItem', content: string, fee: number, txid: string, block: { __typename?: 'Block', createdAt: string, hash: string, height: number }, topic: { __typename?: 'Topic', name: string, hex: string, createdAt: string } }> };
+export type TopicsQueryVariables = Exact<{ [key: string]: never }>
 
-export type TopicsQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type TopicsQuery = { __typename?: 'Query', topics: Array<{ __typename?: 'Topic', hex: string, name: string }> };
-
+export type TopicsQuery = {
+  __typename?: 'Query'
+  topics: Array<{ __typename?: 'Topic'; hex: string; name: string }>
+}
 
 export const NewsDocument = gql`
-    query News($topic: String!) {
-  newsByTopic(topic: $topic) {
-    content
-    fee
-    txid
-    block {
-      createdAt
-      hash
-      height
-    }
-    topic {
-      name
-      hex
-      createdAt
+  query News($topic: String!) {
+    newsByTopic(topic: $topic) {
+      content
+      fee
+      txid
+      block {
+        createdAt
+        hash
+        height
+      }
+      topic {
+        name
+        hex
+        createdAt
+      }
     }
   }
-}
-    `;
+`
 
 /**
  * __useNewsQuery__
@@ -97,25 +122,32 @@ export const NewsDocument = gql`
  *   },
  * });
  */
-export function useNewsQuery(baseOptions: Apollo.QueryHookOptions<NewsQuery, NewsQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<NewsQuery, NewsQueryVariables>(NewsDocument, options);
-      }
-export function useNewsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<NewsQuery, NewsQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<NewsQuery, NewsQueryVariables>(NewsDocument, options);
-        }
-export type NewsQueryHookResult = ReturnType<typeof useNewsQuery>;
-export type NewsLazyQueryHookResult = ReturnType<typeof useNewsLazyQuery>;
-export type NewsQueryResult = Apollo.QueryResult<NewsQuery, NewsQueryVariables>;
-export const TopicsDocument = gql`
-    query Topics {
-  topics {
-    hex
-    name
-  }
+export function useNewsQuery(
+  baseOptions: Apollo.QueryHookOptions<NewsQuery, NewsQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<NewsQuery, NewsQueryVariables>(NewsDocument, options)
 }
-    `;
+export function useNewsLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<NewsQuery, NewsQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<NewsQuery, NewsQueryVariables>(
+    NewsDocument,
+    options
+  )
+}
+export type NewsQueryHookResult = ReturnType<typeof useNewsQuery>
+export type NewsLazyQueryHookResult = ReturnType<typeof useNewsLazyQuery>
+export type NewsQueryResult = Apollo.QueryResult<NewsQuery, NewsQueryVariables>
+export const TopicsDocument = gql`
+  query Topics {
+    topics {
+      hex
+      name
+    }
+  }
+`
 
 /**
  * __useTopicsQuery__
@@ -132,14 +164,27 @@ export const TopicsDocument = gql`
  *   },
  * });
  */
-export function useTopicsQuery(baseOptions?: Apollo.QueryHookOptions<TopicsQuery, TopicsQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<TopicsQuery, TopicsQueryVariables>(TopicsDocument, options);
-      }
-export function useTopicsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<TopicsQuery, TopicsQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<TopicsQuery, TopicsQueryVariables>(TopicsDocument, options);
-        }
-export type TopicsQueryHookResult = ReturnType<typeof useTopicsQuery>;
-export type TopicsLazyQueryHookResult = ReturnType<typeof useTopicsLazyQuery>;
-export type TopicsQueryResult = Apollo.QueryResult<TopicsQuery, TopicsQueryVariables>;
+export function useTopicsQuery(
+  baseOptions?: Apollo.QueryHookOptions<TopicsQuery, TopicsQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useQuery<TopicsQuery, TopicsQueryVariables>(
+    TopicsDocument,
+    options
+  )
+}
+export function useTopicsLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<TopicsQuery, TopicsQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+  return Apollo.useLazyQuery<TopicsQuery, TopicsQueryVariables>(
+    TopicsDocument,
+    options
+  )
+}
+export type TopicsQueryHookResult = ReturnType<typeof useTopicsQuery>
+export type TopicsLazyQueryHookResult = ReturnType<typeof useTopicsLazyQuery>
+export type TopicsQueryResult = Apollo.QueryResult<
+  TopicsQuery,
+  TopicsQueryVariables
+>
