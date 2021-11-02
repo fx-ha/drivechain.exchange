42b622b1cae9f855cd175196aec546ed68ac617c
Felix Ha <felixha@protonmail.com>
2021-11-02 05:57:43 +0000

    feat(web): :sparkles: get graphql url from env

--- web/src/pages/news.tsx
+++ web/src/pages/news.tsx
@@ -26,7 +26,7 @@ import { replaceUrls } from '../utils'
 import { NewsItem, useNewsQuery, useTopicsQuery } from '../generated/graphql'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
+  uri: process.env.NEXT_PUBLIC_API_URL as string,
   cache: new InMemoryCache(),
 })
 
