7203610c98ed1e06828bd764b371add6c950b581
Felix Ha <felixha@protonmail.com>
2021-11-24 00:19:44 +0000

    feat(web): :sparkles: show topic in route

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/post',
+        destination: '/news/a1a1a1a1',
+        permanent: true,
+      },
+      {
+        source: '/news',
+        destination: '/news/a1a1a1a1',
+        permanent: true,
+      },
+    ]
+  },
 }
